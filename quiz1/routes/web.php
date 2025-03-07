064fce1979b7741a2b0e12695901de023d5326ef
AngeloGerardTerrible <144972042+AngeloGerardTerrible@users.noreply.github.com>
2025-03-07 04:01:36 +0000

    update: index.blade.php,create.blade.php,show.blade.php, edit.blade.php

--- quiz1/routes/web.php
+++ quiz1/routes/web.php
@@ -10,3 +10,5 @@ Route::get('hello/{Angelo}', function($name) {
     return 'hi ' . $name; // It will say "hi" followed by whatever name you typed (like "hi John")
 
 });
+
+Route::resource('/inventory', InventoryController::class);
